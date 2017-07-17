53d1cadfa93ab76b20dcd8a9444675970627b657
Pilip_Lipnitski <Pilip_Lipnitski@EPBYMINW1071>
2017-07-17 15:14:42 +0300

    Custom exeptions and exception filter.

---
+++ Business.Interfacies/Exceptions/XmlValidationException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Business.Interfacies.Exceptions
+{
+    public class XmlValidationException : Exception
+    {
+        public XmlValidationException()
+        {
+
+        }
+
+        public XmlValidationException(string message) : base(message)
+        {
+
+        }
+
+        public XmlValidationException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
--- Business/Validator/XmlValidator.cs
+++ Business/Validator/XmlValidator.cs
@@ -1,10 +1,43 @@
-﻿namespace Business.Validator
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+using Business.Interfacies.Exceptions;
+
+namespace Business.Validator
 {
     public class XmlValidator
     {
-        public void ValidateXmlScheme()
+        private static readonly string xsdMarkup =
+            @"<?xml version='1.0'?>
+                                    <xsd:schema xmlns:xsd='http://www.w3.org/2001/XMLSchema'>
+	                                    <xsd:element name='ubf'>
+		                                    <xsd:complexType>  
+			                                    <xsd:sequence>  
+				                                    <xsd:element name='PayloadId' type='xsd:string'/>    
+			                                    </xsd:sequence>  
+		                                    </xsd:complexType>  
+	                                    </xsd:element>
+                                    </xsd:schema>";
+
+        private readonly XmlSchemaSet schemaSet;
+        public XmlValidator()
         {
-            
+            schemaSet = new XmlSchemaSet();
+            schemaSet.Add("", XmlReader.Create(new StringReader(xsdMarkup)));
+        }
+
+        public void ValidateXmlScheme(XDocument xml)
+        {
+            try
+            {
+                xml.Validate(schemaSet, null);
+            }
+            catch (XmlSchemaValidationException e)
+            {
+                throw new XmlValidationException("Xml scheme validation error.", e);
+            }
         }
     }
 }
